d0485783899a88767fe01f4b08ae8cc60236c4dd
rx-ted <1552488649@qq.com>
2025-07-14 20:56:37 +0800

    update the project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,61 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  tseslint.configs.recommended,
-  pluginVue.configs["flat/essential"],
-  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
-]);
+export default antfu({
+}, {
+  ignores: [
+    '*.js',
+    'docs/notes/**/*.md',
+    'docs/.vitepress/cache',
+  ],
+}, {
+  rules: {
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/extensions': 'off',
+    'quotes': [
+      'warn',
+      'single',
+    ],
+    'semi': [
+      'warn',
+      'never',
+    ],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-restricted-syntax': 'off',
+    'no-bitwise': 'off',
+    'camelcase': 'off',
+    'no-case-declarations': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'no-undef': 'off',
+    'no-shadow': 'off',
+    'max-len': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-use-before-define': 'off',
+    'no-continue': 'off',
+    'no-extend-native': 'warn',
+    'no-underscore-dangle': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'non-null-expression': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'consistent-return': 'off',
+    'no-console': 'off',
+    'ts/comma-dangle': 'off',
+    'curly': 'off',
+    'unicorn/prefer-node-protocol': 'off',
+    'ts/no-var-requires': 'off',
+    'ts/no-require-imports': 'off',
+    'ts/ban-ts-comment': 'off',
+    'vue/prefer-import-from-vue': 'off',
+    'jsdoc/no-types': 'off',
+    'no-template-curly-in-string': 'off',
+    'use-isnan': 'warn',
+    'unicorn/prefer-number-properties': 'off',
+    'no-unneeded-ternary': 'off',
+    'vue/require-component-is': 'off',
+  },
+})
